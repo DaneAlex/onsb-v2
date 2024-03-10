2c852a70a4edfd866f323024cc57d904e2217192
Dane <32422882+DaneAlex@users.noreply.github.com>
2024-03-09 22:29:37 -0600

    update description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'ONSB';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'My blog centered around automation, devops, and cloud technologies.';
